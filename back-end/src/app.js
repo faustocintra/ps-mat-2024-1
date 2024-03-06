e9aabb63cde9515def8df291e198025ea87b181e
Luna <zluna.dev@gmail.com>
2024-03-06 12:13:29 -0300

    aula 06/03 crud users e customer

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 
 import indexRouter from "./routes/index.js";
-import usersRouter from "./routes/users.js";
+//import usersRouter from "./routes/users.js";
 
 const app = express();
 
@@ -13,13 +13,18 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+//app.use("/users", usersRouter);
 
 /************************************************
  * ROTAS DA API
 ************************************************/
 
-import carRouter from './routes/car.js'
-app.use('/cars', carRouter)
+import carRoute from './routes/car.js'
+app.use('/cars', carRoute)
 
+import userRoute from './routes/user.js'
+app.use('/users', userRoute)
+
+import customerRoute from './routes/customer.js'
+app.use('/customer', customerRoute)
 export default app;
