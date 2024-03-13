0b6df73f871a0c8953c34b02e3155f691fe14cfd
LoliH02 <giovannacerino.mend@gmail.com>
2024-03-13 10:06:00 -0300

    13/03 - Atividade e prints

---
+++ back-end/src/app.js
@@ -0,0 +1,29 @@
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import indexRouter from "./routes/index.js";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+
+/************************************************
+ * ROTAS DA API
+************************************************/
+
+import carRoute from './routes/car.js'
+app.use('/cars', carRoute)
+
+import usersRoute from './routes/users.js'
+app.use('/users', usersRoute)
+
+import customerRoute from './routes/customer.js'
+app.use('/customers', customerRoute)
+
+export default app;
---
+++ back-end/src/controllers/user.js
@@ -0,0 +1,129 @@
+// Importando o Prisma Client
+import prisma from '../database/client.js'
+import bcrypt from 'bcrypt'
+
+const controller = {}   // Objeto vazio
+
+// Criando um novo user
+controller.create = async function (req, res) {
+  try {
+
+    // Se o campo "password" tiver sido passado dentro
+    // de req.body, é necessário criptografar a senha.
+    // Isso é feito com a biblioteca bcrypt, usando
+    // 12 passos de criptografia
+    if (req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 12)
+    }
+
+    await prisma.user.create({ data: req.body })
+
+    // HTTP 201: Created
+    res.status(201).end()
+  }
+  catch(error) {
+    console.log(error)
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+// Mostrando todos os users
+controller.retrieveAll = async function (req, res) {
+  try {
+    const result = await prisma.user.findMany()
+
+    // Exclui o campo "password" antes de enviar os dados para o cliente
+    for (let user of result) {
+      if(user.password) delete user.password
+    }
+
+    // HTTP 200: OK (implícito)
+    res.send(result)
+
+  }
+  catch(error) {
+    console.log(error)
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+// Mostrando um user por id
+controller.retrieveOne = async function(req, res) {
+
+  try {
+    const result = await prisma.user.findUnique({
+      where: { id: Number(req.params.id) }
+    })
+
+    // Exclui o campo "password" antes de enviar os dados
+    // para o cliente
+    if (result.password) delete result.password
+
+    // Encontrou: retorna HTTP 200: OK
+    if(result) res.send(result)
+    // Não encontrou: retorna HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.log(error)
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+// Atualizando/modificando um user
+controller.update = async function(req, res) {
+  try {
+
+    // Se o campo "password" tiver sido passado dentro
+    // de req.body, é necessário criptografar a senha.
+    // Isso é feito com a biblioteca bcrypt, usando
+    // 12 passos de criptografia
+    if (req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 12)
+    }
+
+    const result = await prisma.user.update({
+      where: { id: Number(req.params.id) },
+      data: req.body
+    })
+
+    // Encontrou e atualizou: retorna HTTP 204: No Content
+    if(result) res.status(204).end()
+    // Não encontrou (e não atualizou): retorna HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.log(error)
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+// Removendo um user
+controller.delete = async function(req, res) {
+  try {
+    const result = await prisma.user.delete({
+      where: { id: Number(req.params.id) }
+    })
+
+    // Encontrou e excluiu: retorna HTTP 204: No Content
+    if(result) res.status(204).end()
+    // Não encontrou (e não excluiu): retorna HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.log(error)
+
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+export default controller
