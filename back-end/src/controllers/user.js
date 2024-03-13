9bf6d4bb679cc059a7eb443c157bc4cf650720db
ElianeO <elianecristina_d@yahoo.com>
2024-03-13 10:52:59 -0300

    (13/03) Início da autorização

--- back-end/src/controllers/user.js
+++ back-end/src/controllers/user.js
@@ -1,6 +1,7 @@
 // Importando o Prisma Client
 import prisma from '../database/client.js'
 import bcrypt from 'bcrypt'
+import  jwt  from 'jsonwebtoken'
 
 const controller = {}   // Objeto vazio
 
@@ -12,7 +13,7 @@ controller.create = async function (req, res) {
     // dentro de req.body, é necessário criptografar
     // a senha. Isso é feito com a biblioteca bcrypt,
     // usando 12 passos de criptografia
-    if(req.body.password) {
+    if (req.body.password) {
       req.body.password = await bcrypt.hash(req.body.password, 12)
     }
 
@@ -21,7 +22,7 @@ controller.create = async function (req, res) {
     // HTTP 201: Created
     res.status(201).end()
   }
-  catch(error) {
+  catch (error) {
     console.log(error)
 
     // HTTP 500: Internal Server Error
@@ -35,15 +36,15 @@ controller.retrieveAll = async function (req, res) {
 
     // Exclui o campo "password" antes de enviar os dados
     // para o cliente
-    for(let user of result) {
-      if(user.password) delete user.password
+    for (let user of result) {
+      if (user.password) delete user.password
     }
 
     // HTTP 200: OK (implícito)
     res.send(result)
 
   }
-  catch(error) {
+  catch (error) {
     console.log(error)
 
     // HTTP 500: Internal Server Error
@@ -51,7 +52,7 @@ controller.retrieveAll = async function (req, res) {
   }
 }
 
-controller.retrieveOne = async function(req, res) {
+controller.retrieveOne = async function (req, res) {
   try {
     const result = await prisma.user.findUnique({
       where: { id: Number(req.params.id) }
@@ -59,14 +60,14 @@ controller.retrieveOne = async function(req, res) {
 
     // Exclui o campo "password" antes de enviar os dados
     // para o cliente
-    if(result.password) delete result.password
+    if (result.password) delete result.password
 
     // Encontrou: retorna HTTP 200: OK
-    if(result) res.send(result)
+    if (result) res.send(result)
     // Não encontrou: retorna HTTP 404: Not Found
     else res.status(404).end()
   }
-  catch(error) {
+  catch (error) {
     console.log(error)
 
     // HTTP 500: Internal Server Error
@@ -74,14 +75,14 @@ controller.retrieveOne = async function(req, res) {
   }
 }
 
-controller.update = async function(req, res) {
+controller.update = async function (req, res) {
   try {
 
     // Se o campo "password" tiver sido passado
     // dentro de req.body, é necessário criptografar
     // a senha. Isso é feito com a biblioteca bcrypt,
     // usando 12 passos de criptografia
-    if(req.body.password) {
+    if (req.body.password) {
       req.body.password = await bcrypt.hash(req.body.password, 12)
     }
 
@@ -91,11 +92,11 @@ controller.update = async function(req, res) {
     })
 
     // Encontrou e atualizou: retorna HTTP 204: No Content
-    if(result) res.status(204).end()
+    if (result) res.status(204).end()
     // Não encontrou (e não atualizou): retorna HTTP 404: Not Found
     else res.status(404).end()
   }
-  catch(error) {
+  catch (error) {
     console.log(error)
 
     // HTTP 500: Internal Server Error
@@ -110,15 +111,59 @@ controller.delete = async function (req, res) {
     })
 
     // Encontrou e excluiu ~> HTTP 204: No Content
-    if(result) res.status(204).end()
+    if (result) res.status(204).end()
     // Não encontrou (e não excluiu) ~> HTTP 404: Not Found
     else res.status(404).end()
   }
-  catch(error) {
+  catch (error) {
     console.log(error)
 
     // HTTP 500: Internal Server Error
     res.status(500).end()
+  }
+}
+
+controller.login = async function (req, res) {
+  try {
+
+    // Busca o usuário pelo e-mail passado
+    const user = await prisma.user.findUnique({
+      where: { email: req.body?.email }
+    })
+
+    // Se o usupário não for encontrado, retorna
+    // HTTP 401: Unauthorized
+    if (!user) return res.send(401).end()
+
+    // Usuário encontrado, conferimos a senha
+    const passwordOK = await bcrypt.compare(req.body.password, user.password)
+
+    // Senha errada, retorna 
+    // HTTP401: Unauthorized
+    if (!passwordOK) return res.send(401).end()
+
+    // Usuário e senha onkeydown, passamos ao procedimento de gerar o token
+    
+    // Excluímos o campo "password" do usuário, para que ele não seja incluído no token
+    if(user.password) delete user.password
+
+    // Geração do token
+    const token = jwt.sign(
+      user,                     // Dados do usuário
+      process.env.TOKEN_SECRET,                 // Senha para criptografar o token
+      { expiresIn: '24h'}    // Prazo de validade do token
+    )
+
+    // Retorna HTTP 200: OK com o token
+    res.send({token})
+  }
+
+  catch (error) {
+    console.error(error)
+    //HTTP 500: Internal Server Error
+    res.status(500).end()
+
+
   }
 }
 
