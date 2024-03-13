bd38f301f9742c6c6de0d17a3e5cf915442852b1
michele <michele.mmayra@gmail.com>
2024-03-13 09:01:35 -0300

    (13/03) criação das pastas prints

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ps-mat-2024-1
 Repositório da disciplina Eletiva II - Programação de Scripts, 5º semestre ADS matutino Fatec Franca 2024/1
+# Instalação do Prisma 
+npm install prima --save-dev
+
+# Inicialização  do Prisma 
+npx prisma init --datasource-provider postgresql
+
+# Executar uma migration 
+npx prisma migrate dev --name init 
---
+++ back-end/src/controllers/customer.js
@@ -0,0 +1,99 @@
+// Importando o Prisma Client
+import prisma from '../database/client.js'
+
+const controller = {} //Objeto vazio 
+
+//criando um novo carro 
+controller.create = async function (req, res){
+    try {
+      await prisma.customer.create({ data: req.body })
+
+      //HTTP 201: Created 
+      res.status(201).end()
+    }
+    catch(error){
+      console.log(error)
+
+      //HTTP 500: Internal Server Error
+      res.status(500).end()
+    }
+}
+
+controller.retriveAll = async function (req, res) {
+    try {
+     
+        const result = await prisma.customer.findMany()
+
+        //HTTP 200: ok (implícito)
+        res.send(result)
+    }
+    catch(error){
+        console.log(error)
+  
+        //HTTP 500: Internal Server Error
+        res.status(500).end()
+    }
+}
+    controller.retrieveOne = async function(req, res) {
+        try{
+           const result = await prisma.customer.findUnique({ 
+            where: { id: Number(req.params.id) }
+           })
+
+           //Encontrou : retorna HTTP 220: ok
+           if(result)res.send(result)
+           //Não encontrou: retorna HTTP 404: Not Found
+          else res.status(404).end()
+
+        }
+        catch(error){
+            console.log(error)
+      
+            //HTTP 500: Internal Server Error
+            res.status(500).end()
+        }
+
+    }
+
+    controller.update = async function(req, res) {
+        try {
+         const result = await prisma.customer.update({ 
+            where: { id: Number(req.params.id) }, 
+            data: req.body
+         })
+    
+        //Encontrou e atualizou: retorna HTTP 204: No Content 
+        if(result) res.status(204).end()
+        //Não encontrou (e não atualizou): retornar HTTP 404: Not Found
+        else res.status(404).end()
+        }
+        catch(error){
+            console.log(error)
+      
+            //HTTP 500: Internal Server Error
+            res.status(500).end()
+        }
+    }
+
+    controller.delete = async function(req, res) {
+        try {
+            const result = await prisma.customer.delete({ 
+            where: { id: Number(req.params.id) }, 
+
+        })
+
+        //Encontrou e excluiu: retorna HTTP 204: No Content 
+         if(result) res.status(204).end()
+         //Não encontrou (e não excluiu): retornar HTTP 404: Not Found
+         else res.status(404).end()
+        } 
+        catch(error){
+            console.log(error)
+      
+            //HTTP 500: Internal Server Error
+            res.status(500).end()
+        }
+
+    }
+
+export default controller 
