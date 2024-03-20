795e7d34e690e015018b26e55ab50abda35f7c5b
michele <michele.mmayra@gmail.com>
2024-03-20 10:54:49 -0300

    (20/03) Autenticação e criação do projeto front-end

---
+++ front-end/src/App.jsx
@@ -0,0 +1,28 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './App.css'
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './ui/theme'
+
+
+import TopBar from './ui/TopBar'
+
+function App() {
+   
+  return ( 
+    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+       <TopBar/>
+     </ThemeProvider>
+    </>
+    
+     
+  )
+}
+
+export default App
