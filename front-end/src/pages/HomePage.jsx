fcd7d9930e762429db0db99d832f2592a9d7f455
Joel Barini <joepb_008@hotmail.com>
2024-03-27 11:01:02 -0300

    (27/03) Rodapé, navegação e página de login

---
+++ front-end/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+export default function HomePage() {
+    return(
+        <>
+            <Typography variant="h1">
+                Bem-vindo ao sistema Karangos
+            </Typography>
+        </>
+    )
+}
