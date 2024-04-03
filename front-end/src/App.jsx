590d49720a4b4d49323e62159bcb0f020e50ea13
THAYSSA MIGUEL MORTARI LIMA <1090482211011@fatec-srv.local>
2024-04-03 10:54:09 -0300

    (03/04) Término da página de login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,14 @@
+import React from 'react'
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './App.css'
 
-import { ThemeProvider} from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
 import theme from './ui/theme'
-import  CssBaseline  from '@mui/material/CssBaseline';
+import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box'
 
 import { BrowserRouter } from 'react-router-dom'
@@ -14,20 +16,26 @@ import AppRoutes from './routes/AppRoutes';
 
 import TopBar from './ui/TopBar'
 import FooterBar from './ui/FooterBar';
+import AuthUserContext from './contexts/AuthUserContext'
 
 function App() {
+  //Armazena globalmente as informações do usuário autenticado
+  const [authUser, setAuthUser] = React.useState(null)
+
   return (
     <>
-     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-         <CssBaseline/>
-         <TopBar />
-         <Box sx={{ margin: '24px 24px 72px 24px'}}>
-           <AppRoutes />
-         </Box>
-         <FooterBar />
-      </BrowserRouter>
-     </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <CssBaseline />
+          <AuthUserContext.Provider value={{ authUser, setAuthUser }}>
+            <TopBar />
+            <Box sx={{ margin: '24px 24px 72px 24px' }}>
+              <AppRoutes />
+            </Box>
+            <FooterBar />
+          </AuthUserContext.Provider>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   )
 }
