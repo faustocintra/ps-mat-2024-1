020968ecec960edaa47f6c4667cd98df80c65ee2
ADRIANO RODRIGUES GOMES DA SILVA <1090482211010@fatec-srv.local>
2024-04-10 09:11:09 -0300

    (10/04) Verificação de usuário autenticado dessa vez com certeza

--- front-end/src/routes/AuthRoute.jsx
+++ front-end/src/routes/AuthRoute.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Route, useNavigate } from "react-router-dom";
-import myfetch from "../lib/myfetch";
-
-export default function AuthRoute({:...props}) {
+import myfetch from "../lib/myfetch"
+export default async function AuthRoute({...props}) {
     const navigate = useNavigate()
     try {
         //Verifica se o usuário AINDA está autenticado
         await myfetch.get('/users/me')
         //Usuario ainda esta autenticado, segue a vida,
         //retorna a rota normal
-        
         return <Route {...props} />
     }
     catch(error){
+        //Deu Erro: o Usuario não esta mais logado
+        //Redirecionamos para a pagina de login
+        console.error(error)
+        navigate('/login')
 
     }
 }
