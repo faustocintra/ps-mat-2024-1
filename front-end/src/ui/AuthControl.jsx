967a90f340bb939d536aa93faafc1b723a77d1fb
THAYSSA MIGUEL MORTARI LIMA <1090482211011@fatec-srv.local>
2024-04-10 09:07:02 -0300

    (10/04) Verificação de usuário autenticado

---
+++ front-end/src/ui/AuthControl.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import Typography from "@mui/material/Typography"
+import Button from '@mui/material/Button'
+import AuthUserContext from '../contexts/AuthUserContext'
+import { Link } from "react-router-dom"
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+export default function AuthControl() {
+    const { authUser } = React.useContext(AuthUserContext)
+
+    if (authUser) {
+        return (
+            <>
+                <AccountCircleIcon color="secondary" fontsize="small" sx={{ mr: 1 }} />
+                <Typography>
+                    {authUser.fullname}
+                </Typography>
+                <Button color="secondary">Sair</Button>
+
+            </>
+        )
+    }
+    else {
+        return (
+            <Link to="/login">
+                <Button color="secondary">Entrar</Button>
+            </Link>
+        )
+    }
+
+}
--- front-end/src/ui/TopBar.jsx
+++ front-end/src/ui/TopBar.jsx
@@ -2,10 +2,9 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-
 import logo from '../assets/karangos-logo-600px.png'
-import MainMenu from './MainMenu';
+import MainMenu from './MainMenu'
+import AuthControl from './AuthControl'
 
 export default function TopBar() {
   return (
@@ -13,10 +12,10 @@ export default function TopBar() {
       <AppBar position="static" enableColorOnDark>
         <Toolbar>
           <MainMenu />
-          <Box sx={{ flexGrow: 1}}>
-            <img src={logo} alt="Logotipo Karangos" style={{ width: '300px'}} />
+          <Box sx={{ flexGrow: 1 }}>
+            <img src={logo} alt="Logotipo Karangos" style={{ width: '300px '}} />
           </Box>
-          <Button color="inherit">Login</Button>
+          <AuthControl />
         </Toolbar>
       </AppBar>
     </Box>
