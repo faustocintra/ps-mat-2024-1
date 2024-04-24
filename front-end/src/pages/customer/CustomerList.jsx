5f06c4af69346862c3c7f5e9b82825691178831a
HEITOR RAMOS NOGUEIRA <1090482211033@fatec-srv.local>
2024-04-24 11:04:21 -0300

    (24/04) inicio do formulario dos clientes

--- front-end/src/pages/customer/CustomerList.jsx
+++ front-end/src/pages/customer/CustomerList.jsx
@@ -4,45 +4,79 @@ import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 import myfetch from '../../lib/myfetch'
 import Waiting from '../../ui/Waiting'
-
-const columns = [
-  { 
-    field: 'id', 
-    headerName: 'Cód.', 
-    type: 'number',
-    width: 80 
-  },
-  {
-    field: 'name',
-    headerName: 'Nome',
-    width: 250
-  },
-  {
-    field: 'ident_document',
-    headerName: 'CPF',
-    width: 150
-  },
-  {
-    field: 'municipality',
-    headerName: 'Município/UF',
-    width: 200,
-    // Colocando dois campos na mesma célula
-    valueGetter: (value, row) => row.municipality + '/' + row.state
-  },
-  {
-    field: 'phone',
-    headerName: 'Tel./celular',
-    width: 160
-  },
-  {
-    field: 'email',
-    headerName: 'E-mail',
-    width: 250
-  },
-];
+import IconButton  from '@mui/material/IconButton'
+import Paper  from '@mui/material/Paper'
+import Button  from '@mui/material/Button'
+import { Link } from 'react-router-dom';
+import AddBoxIcon from '@mui/icons-material/AddBox'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export default function CustomerList() {
 
+  const columns = [
+    { 
+      field: 'id', 
+      headerName: 'Cód.', 
+      type: 'number',
+      width: 80 
+    },
+    {
+      field: 'name',
+      headerName: 'Nome',
+      width: 250
+    },
+    {
+      field: 'ident_document',
+      headerName: 'CPF',
+      width: 150
+    },
+    {
+      field: 'municipality',
+      headerName: 'Município/UF',
+      width: 200,
+      // Colocando dois campos na mesma célula
+      valueGetter: (value, row) => row.municipality + '/' + row.state
+    },
+    {
+      field: 'phone',
+      headerName: 'Tel./celular',
+      width: 160
+    },
+    {
+      field: 'email',
+      headerName: 'E-mail',
+      width: 250
+    },
+    {
+      field: '_edit',
+      headerName: 'Editar',
+      headerAlign: 'center',
+      align: 'center',
+      sortable: 'false',
+      width: 90,
+      renderCell: params => (
+        <Link to={`./${params.id}`}>
+          <IconButton aria-label="Editar">
+            <EditIcon />
+          </IconButton>
+        </Link>
+      )
+    },
+    {
+      field: '_delete',
+      headerName: 'Excluir',
+      headerAlign: 'center',
+      align: 'center',
+      sortable: 'false',
+      width: 90,
+      renderCell: params => (
+          <IconButton aria-label="Excluir" onClick={() => handleDeleteButtonClick(params.id)}>
+            <DeleteForeverIcon color="error" />
+          </IconButton>
+      )
+    }
+  ];
   const [state, setState] = React.useState({
     customers: [],
     showWaiting: false
@@ -80,12 +114,54 @@ export default function CustomerList() {
     }
   }
 
+  async function handleDeleteButtonClick(deleteId){
+    if(confirm('Deseja realmente excluir este item?')){
+      //exibe a tela de espera
+      setState({...state, showWaiting: true})
+      try{
+        await myfetch.delete(`/customers/${deleteId}`)
+
+        //recarrega os dados da grid
+        fetchData()
+        alert('Item excluído com sucesso.')
+
+        //esconde a tela de espera
+        setState({...setState, showWaiting: false})
+      }
+      catch(error){
+        alert(error.message)
+
+        //esconde a tela de espera
+        setState({...setState, showWaiting: false})
+         }
+       }
+     }
   return(
     <>
+      <Waiting show={showWaiting} />
+
       <Typography variant="h1" gutterBottom>
         Listagem de clientes
       </Typography>
 
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'right',
+        mb: 2 //marginbuttom
+      }}>
+        <Link to="./new">
+          <Button
+          variant="contained"
+          size="large"
+          color="secondary"
+          startIcon={<AddBoxIcon />}
+          >
+            Novo cliente
+          </Button>
+        </Link>
+      </Box>
+
+      <Paper elevation={10}>
       <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
         rows={customers}
@@ -99,7 +175,8 @@ export default function CustomerList() {
         }}
         pageSizeOptions={[5]}
       />
-    </Box>
+      </Box>
+    </Paper>
     </>
   )
 }
