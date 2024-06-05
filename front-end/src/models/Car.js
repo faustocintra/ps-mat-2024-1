93aa185c4800a447c839abb83495e53dbe1abaa1
THAYSSA MIGUEL MORTARI LIMA <1090482211011@fatec-srv.local>
2024-06-05 09:12:33 -0300

    (05/06) Finalização do TRABALHO 2

--- front-end/src/models/Car.js
+++ front-end/src/models/Car.js
@@ -1,21 +1,62 @@
+// import { z } from 'zod';
+
+// const currentYear = new Date().getFullYear();
+
+// const CarSchema = z.object({
+//   brand: z.string().max(25),
+//   model: z.string().max(25),
+//   color: z.string().max(12),
+//   year_manufacture: z.number().int().min(1960).max(currentYear),
+//   imported: z.boolean(),
+//   selling_date: z.date().max(new Date(), { message: 'A data de venda não pode estar no futuro.' }).optional(),
+//   selling_price:
+//     z.coerce.number()
+//     .min(1000, { message: 'O preço de venda deve ser maior ou igual a 1.000' })
+//     .max(5000000, { message: 'O preço de venda deve ser menor ou igual a 5.000.000' })
+//     .nullable(),
+// })
+
+// export default CarSchema;
+
+
 import { z } from 'zod';
 
-const currentYear = new Date().getFullYear();
+const currentYear = new Date().getFullYear()  //hoje
+
+
 
 const CarSchema = z.object({
-  brand: z.string().max(25),
-  model: z.string().max(25),
-  color: z.string().max(12),
-  year_manufacture: z.number().int().min(1960).max(currentYear),
-  imported: z.boolean(),
-  selling_date: z.date().max(new Date(), { message: 'A data de venda não pode estar no futuro.' }).optional(),
+  brand: 
+    z.string()
+    .max(25, { message: 'A marca deve ter, no máximo, 25 caracteres' }),
+  model: 
+   z.string()
+   .max(25, { message: 'A marca deve ter no máximo, 25 caracteres'}),
+  color: z.string()
+   .max(12, { message: 'A cor precisa ter, no máximo, 12 caracteres'}),
+
+  year_manufacture: 
+   z.number()
+   .int( { message: 'O ano de fabricação precisa ser um número inteiro'})
+   .min(1960, {message: 'O ano de fabricação precisa ser no mínimo em 1960'})
+   .max(currentYear, {message: 'O ano de fabricação precisa ser até o ano atual'}),
+
+  imported: 
+   z.coerce.boolean(),
+
+  selling_date:
+    z.coerce.date()
+    .refine(date => date <= new Date(), { message: 'A data de venda não pode estar no futuro' })
+    .nullable(),
+      
   selling_price:
     z.coerce.number()
-    .min(1000, { message: 'O preço de venda deve ser maior ou igual a 1.000' })
-    .max(5000000, { message: 'O preço de venda deve ser menor ou igual a 5.000.000' })
-    .nullable(),
+      .min(1000, { message: 'O preço de venda deve ser maior ou igual a 1.000' })
+      .max(5000000, { message: 'O preço de venda deve ser menor ou igual a 5.000.000' })
+      .nullable(),
 })
 
 export default CarSchema;
+
 
 
--- front-end/src/pages/customer/CarForm.jsx
+++ front-end/src/pages/customer/CarForm.jsx
@@ -1,251 +1,3 @@
-// import React from 'react';
-// import Typography from '@mui/material/Typography';
-// import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
-// import Checkbox from '@mui/material/Checkbox';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import InputMask from 'react-input-mask';
-// import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
-// import { ptBR } from 'date-fns/locale/pt-BR';
-// import MenuItem from '@mui/material/MenuItem';
-// import Button from '@mui/material/Button';
-// import useConfirmDialog from '../../ui/useConfirmDialog';
-// import useNotification from '../../ui/useNotification';
-// import useWaiting from '../../ui/useWaiting';
-// import CarSchema from '../../models/Car';
-// import myfetch from '../../lib/myfetch';
-
-// export default function CarForm() {
-//   const [state, setState] = React.useState({
-//     car: {
-//       brand: '',
-//       model: '',
-//       color: '',
-//       year_manufacture: new Date().getFullYear(),
-//       imported: false,
-//       plates: '',
-//       selling_date: null,
-//       selling_price: '',
-//     },
-//     formModified: false,
-//     inputErrors: {},
-//   });
-
-//   const { car, formModified, inputErrors } = state;
-
-//   const { askForConfirmation, ConfirmDialog } = useConfirmDialog();
-//   const { notify, Notification } = useNotification();
-//   const { showWaiting, Waiting } = useWaiting();
-
-//   const colors = [
-//     {value: 'Azul', label: 'Azul'},
-//     {value: 'Branco', label: 'Branco'},
-//     {value: 'Cinza', label: 'Cinza'},
-//     {value: 'Preto', label: 'Preto'},
-//     {value: 'Verde', label: 'Verde'},
-//     {value: 'Vermelho', label: 'Vermelho'}
-//   ]
-
-//   const handleFieldChange = (event) => {
-//     const { name, value, checked, type } = event.target;
-//     const newValue = type === 'checkbox' ? checked : value;
-//     setState((prevState) => ({
-//       ...prevState,
-//       car: {
-//         ...prevState.car,
-//         [name]: newValue,
-//       },
-//       formModified: true,
-//     }));
-//   };
-
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-//     showWaiting(true);
-//     try {
-//       CarSchema.parse(car);
-//        await myfetch.post('/cars', car);
-//       notify('Carro salvo com sucesso', 'success');
-//     } catch (error) {
-//       console.error(error);
-//       if (error.errors) {
-//         const messages = {};
-//         error.errors.forEach((err) => {
-//           messages[err.path[0]] = err.message;
-//         });
-//         setState((prevState) => ({
-//           ...prevState,
-//           inputErrors: messages,
-//         }));
-//         notify('Existem campos inválidos no formulário.', 'error');
-//       } else {
-//         notify(error.message, 'error');
-//       }
-//     } finally {
-//       showWaiting(false);
-//     }
-//   };
-
-//   return (
-//     <>
-//       <ConfirmDialog />
-//       <Notification />
-//       <Waiting />
-
-//       <Typography variant="h1" gutterBottom>
-//         Car Form
-//       </Typography>
-
-//       <Box className="form-fields">
-//         <form onSubmit={handleFormSubmit}>
-//           <TextField
-//             name="brand"
-//             label="Brand"
-//             variant="filled"
-//             required
-//             fullWidth
-//             value={car.brand}
-//             onChange={handleFieldChange}
-//             helperText={inputErrors?.brand}
-//             error={inputErrors?.brand}
-//           />
-
-//           <TextField
-//             name="model"
-//             label="Model"
-//             variant="filled"
-//             required
-//             fullWidth
-//             value={car.model}
-//             onChange={handleFieldChange}
-//             helperText={inputErrors?.model}
-//             error={inputErrors?.model}
-//           />
-
-//           <TextField
-//             name="color"
-//             label="Cor"
-//             variant="filled"
-//             required
-//             fullWidth
-//             value={car.color}
-//             onChange={handleFieldChange}
-//             select
-//             helperText={inputErrors?.color}
-//             error={inputErrors?.color}
-//           >
-//             {/* {colors.map((color) => {
-//                 <MenuItem key={color.value} value={color.value}>
-//                     {color.label}
-//                 </MenuItem>
-//             })} */}
-
-//             {
-//                 colors.map(c =>{
-//                     <MenuItem key={c.value} value={c.value}>
-//                         {c.label}
-//                     </MenuItem>
-//                 })
-//             }
-//           </TextField>        
-
-
-//           <TextField
-//             name="year_manufacture"
-//             label="Year of Manufacture"
-//             variant="filled"
-//             required
-//             select
-//             fullWidth
-//             value={car.year_manufacture}
-//             onChange={handleFieldChange}
-//             helperText={inputErrors?.year_manufacture}
-//             error={inputErrors?.year_manufacture}
-//           >
-//             {Array.from({ length: new Date().getFullYear() - 1960 + 1 }, (_, i) => (
-//               <MenuItem key={i} value={new Date().getFullYear() - i}>
-//                 {new Date().getFullYear() - i}
-//               </MenuItem>
-//             ))}
-//           </TextField>
-
-//           <InputMask
-//             mask="AAA-9$99"
-//             value={car.plates}
-//             onChange={handleFieldChange}
-//           >
-//             {() => (
-//               <TextField
-//                 name="plates"
-//                 label="Plates"
-//                 variant="filled"
-//                 required
-//                 fullWidth
-//                 helperText={inputErrors?.plates}
-//                 error={inputErrors?.plates}
-//               />
-//             )}
-//           </InputMask>
-
-//           <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptBR}>
-//             <DatePicker
-//               name="selling_date"
-//               label="Selling Date"
-//               value={car.selling_date}
-//               onChange={(value) => handleFieldChange({ target: { name: 'selling_date', value } })}
-//               renderInput={(params) => (
-//                 <TextField
-//                   {...params}
-//                   fullWidth
-//                   variant="filled"
-//                   helperText={inputErrors?.selling_date}
-//                   error={inputErrors?.selling_date}
-//                 />
-//               )}
-//             />
-//           </LocalizationProvider>
-
-//           <TextField
-//             name="selling_price"
-//             label="Selling Price"
-//             variant="filled"
-//             required
-//             fullWidth
-//             value={car.selling_price}
-//             onChange={handleFieldChange}
-//             helperText={inputErrors?.selling_price}
-//             error={inputErrors?.selling_price}
-//           />
-//           <FormControlLabel
-//             control={
-//               <Checkbox
-//                 checked={car.imported}
-//                 onChange={handleFieldChange}
-//                 name="imported"
-//               />
-//             }
-//             label="Imported"
-//           />
-
-//           <Box sx={{ display: 'flex', justifyContent: 'space-around', width: '100%' }}>
-//             <Button
-//               variant="contained"
-//               color="secondary"
-//               type="submit"
-//             >
-//               Save
-//             </Button>
-//           </Box>
-//         </form>
-//       </Box>
-//     </>
-//   );
-// }
-
-
-
-
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -303,13 +55,14 @@ export default function CarForm() {
         { value: 'Azul', label: 'Azul' },
         { value: 'Branco', label: 'Branco' },
         { value: 'Cinza', label: 'Cinza' },
+        { value: 'Prata', label: 'Prata' },
         { value: 'Preto', label: 'Preto' },
         { value: 'Verde', label: 'Verde' },
         { value: 'Vermelho', label: 'Vermelho' }
     ]
 
     const plateMaskFormatChars = {
-        '$': '[0-9A-Z]',  // letra ou digito
+        '$': '[0-9A-J]',  // letra ou digito de A ao J
         '9': '[0-9]',    // somente dígitos
         'A': '[A-Z]'    // somente letras
     }
@@ -324,13 +77,16 @@ export default function CarForm() {
         event.preventDefault()      // Evita que a página seja recarregada
         showWaiting(true)       // Exibe a tela de espera
         try {
-            
+
+            if (car.imported === 'on') car.imported = true
+            else car.imported = false
+
             CarSchema.parse(car)
 
-            
+
             if (params.id) await myfetch.put(`/cars/${params.id}`, car)
-            
-            else await myfetch.post('/cars', customer)
+
+            else await myfetch.post('/cars', car)
 
             notify('Carro salvo com sucesso.', 'success', 4000, () => {
                 navigate('..', { relative: 'path', replace: true })
@@ -339,7 +95,7 @@ export default function CarForm() {
         catch (error) {
             console.error(error)
             if (error instanceof ZodError) {
-                
+
                 const messages = {}
                 for (let i of error.issues) messages[i.path[0]] = i.message
                 setState({ ...state, inputErrors: messages })
@@ -348,7 +104,6 @@ export default function CarForm() {
             else notify(error.message, 'error')
         }
         finally {
-            // Desliga a tela de espera, seja em caso de sucesso, seja em caso de erro
             showWaiting(false)
         }
     }
@@ -460,19 +215,22 @@ export default function CarForm() {
 
                     <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptBR}>
                         <DatePicker
-                            name="selling_date"
                             label="Data de Venda"
                             value={car.selling_date}
-                            onChange={(value) => handleFieldChange({ target: { name: 'selling_date', value } })}
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    fullWidth
-                                    variant="filled"
-                                    helperText={inputErrors?.selling_date}
-                                    error={inputErrors?.selling_date}
-                                />
-                            )}
+                            onChange={(value) => handleFieldChange({
+                                target: { name: 'selling_date', value }
+                            })}
+                            slotProps={{
+                                textField: {
+                                    fullWidth: true,
+                                    variant: 'filled',
+                                    helperText: inputErrors?.selling_date,
+                                    error: inputErrors?.selling_date
+
+                                }
+                            }}
+
+
                         />
                     </LocalizationProvider>
 
@@ -504,7 +262,13 @@ export default function CarForm() {
                             color="secondary"
                             type="submit"
                         >
-                            Save
+                            Salvar
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            onClick={handleBackButtonClick}
+                        >
+                            Voltar
                         </Button>
                     </Box>
                 </form>
